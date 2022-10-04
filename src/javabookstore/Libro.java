1708ed5ab2fd20950ff84fac8e37013a348d0c5e
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-04 16:53:10 +0200

    caratteristiche del libro aggiunte

---
+++ src/javabookstore/Libro.java
@@ -0,0 +1,12 @@
+package javabookstore;
+
+public class Libro {
+
+	public static void main(String[] args) {
+		String titolo, editore, lingua, isbn;
+		int numPagine;
+		double peso, prezzo;
+		
+	}
+
+}
