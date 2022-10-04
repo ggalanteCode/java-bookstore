b33a1abf04e39f087240512251787692cdd45828
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-04 17:12:38 +0200

    calcolo della percentuale aggiunto

--- src/com/soprasteria/javabookstore/Libro.java
+++ src/com/soprasteria/javabookstore/Libro.java
@@ -7,6 +7,16 @@ public class Libro {
 		int numPagine;
 		double peso, prezzo;
 		
+		prezzo = 34.20;
+		
+		double sconto = 20;
+		
+		double prezzoDaSottrarre = (prezzo/100)*sconto;
+		
+		double prezzoScontato = prezzo - prezzoDaSottrarre;
+		
+		System.out.println("Il prezzo iniziale del libro è: " + prezzo);
+		System.out.println("Il prezzo scontato del libro è: " + prezzoScontato);
 	}
 
 }
